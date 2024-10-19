67c0e9b82794c1e4d735500237ff510549f721b0
AYAZ MAHMOOD <hmayaz528@gmail.com>
2024-10-19 20:36:17 +0500

    leaen: nwsted routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import { Route, Routes } from "react-router";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import { Navbar } from "./components/Navbar";
+import { OrderSummary } from "./components/OrderSummary";
+import { NoMatch } from "./components/NoMatch";
+import { Products } from "./components/Products";
+import { FeaturedProducts } from "./components/FeaturedProducts";
+import { NewProducts } from "./components/NewProducts";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Home</div>
+      <button onClick={() => navigate("order-summary", { replace: true })}>
+        Place Order
+      </button>
+    </>
+  );
 };
